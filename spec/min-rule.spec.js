107a8c547fa024cc4dcf4e18a6c5cd73fee45b21
David <dtang85@gmail.com>
2013-09-27 10:21:17 -0700

    test cleanup

--- spec/min-rule.spec.js
+++ spec/min-rule.spec.js
@@ -7,52 +7,27 @@ describe('min validation rule', function() {
 	var validator;
 
 	it('should fail with the name "D". Minimum size is 2 letters.', function() {
-		validator = new Validator({
-			name: 'D'
-		}, {
-			name: 'min:2'
-		});
-
+		validator = new Validator({ name: 'D' }, { name: 'min:2' });
 		expect(validator.passes()).toBeFalsy();
 	});
 
 	it('should pass with the name "Da". Minimum is 2 letters.', function() {
-		validator = new Validator({
-			name: 'Da'
-		}, {
-			name: 'min:2'
-		});
-
+		validator = new Validator({ name: 'Da' }, { name: 'min:2' });
 		expect(validator.passes()).toBeTruthy();
 	});
 
 	it('should pass with the age "18". Minimum is 18.', function() {
-		validator = new Validator({
-			age: 18
-		}, {
-			age: 'min:18'
-		});
-
+		validator = new Validator({ age: 18 }, { age: 'min:18' });
 		expect(validator.passes()).toBeTruthy();
 	});
 
 	it('should fail with the age "17". Minimum is 18.', function() {
-		validator = new Validator({
-			age: 17
-		}, {
-			age: 'min:18'
-		});
-
+		validator = new Validator({ age: 17 }, { age: 'min:18' });
 		expect(validator.fails()).toBeTruthy();
 	});
 
 	it('should fail with value of 0.04', function() {
-		validator = new Validator({
-			val: 0.04
-		}, {
-			val: 'min:0.05'
-		});
-
+		validator = new Validator({ val: 0.04 }, { val: 'min:0.05' });
 		expect(validator.fails()).toBeTruthy();
 	});
 });
