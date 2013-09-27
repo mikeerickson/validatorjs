48da18964f6d2e2bf700e8c058c9af96f4de71ea
David <dtang85@gmail.com>
2013-09-27 10:16:15 -0700

    adds another test to same rule

--- spec/same-rule.spec.js
+++ spec/same-rule.spec.js
@@ -13,6 +13,13 @@ describe('same validation rule', function() {
 		expect(validator.errors.first('pw2')).toEqual('The pw2 and pw fields must match.');
 	});
 
+	it('should fail when the the comparing attribute doesnt exist', function() {
+		validator = new Validator({ pw2: 'abc1234' }, { pw2: 'same:pw' });
+		expect(validator.fails()).toBeTruthy();
+		expect(validator.passes()).toBeFalsy();
+		expect(validator.errors.first('pw2')).toEqual('The pw2 and pw fields must match.');
+	});
+
 	it('should pass when the 2 attributes are equal', function() {
 		validator = new Validator({ pw: 'abc123', pw2: 'abc123' }, { pw2: 'same:pw' });
 		expect(validator.passes()).toBeTruthy();
