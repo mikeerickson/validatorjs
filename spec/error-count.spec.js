7c960d40b56f7322d5ca4b40783887a67a422e89
Gary Green <holegary@gmail.com>
2015-06-15 11:26:28 +0100

    Added tests for error count

---
+++ spec/error-count.spec.js
@@ -0,0 +1,26 @@
+describe('Error counts', function() {
+	
+	if (typeof require !== 'undefined') {
+		var Validator = require('../src/validator.js');
+	} else {
+		var Validator = window.Validator;
+	}
+
+	it('should return 0 when validation has not yet run', function() {
+		var validator = new Validator({ username: '' }, { username: 'required' });
+		expect(validator.errorCount).toEqual(0);
+	});
+
+	it('should return a count when there are errors', function() {
+		var validator = new Validator({ username: '', name: '' }, { username: 'required', name: 'required' });
+		expect(validator.passes()).toBeFalsy();
+		expect(validator.errorCount).toEqual(2);
+	});
+
+	it('should return not return a count when error free', function() {
+		var validator = new Validator({ username: 'a', name: 'a' }, { username: 'required', name: 'required' });
+		expect(validator.passes()).toBeTruthy();
+		expect(validator.errorCount).toEqual(0);
+	});
+
+});
