c077695e524b4290f4a3fb470d064f8195af052e
Gary Green <holegary@gmail.com>
2015-09-17 17:28:29 +0100

    Remove .spec suffix from test files.

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'dist/validator.js',
-      'spec/*.spec.js'
+      'spec/*.js'
     ],
 
 
