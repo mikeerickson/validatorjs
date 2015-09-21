2d8b2e883fda5a8aaec3a44beb75ada4f7e6a083
Gary Green <holegary@gmail.com>
2015-09-21 16:17:35 +0100

    Bump 2.0.1

--- dist/validator.js
+++ dist/validator.js
@@ -1,4 +1,4 @@
-/*! validatorjs - v2.0.0 -  - 2015-09-19 */
+/*! validatorjs - v2.0.1 -  - 2015-09-21 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Validator = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function AsyncResolvers(onFailedOne, onResolvedAll) {
 	this.onResolvedAll = onResolvedAll;
@@ -225,6 +225,7 @@ var container = {
 	 * @return {void}
 	 */
 	_setRuleMessage: function(lang, attribute, message) {
+		this._load(lang);
 		if (message === undefined) {
 			message = this.messages[lang].def;
 		}
@@ -270,6 +271,7 @@ var container = {
 };
 
 module.exports = container;
+
 },{"./lang/en":5,"./messages":6}],5:[function(require,module,exports){
 module.exports = {
 	accepted: 'The :attribute must be accepted.',
