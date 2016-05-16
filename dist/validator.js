e0a41171218486adcdba06fbb6910e902ba893c2
David Tang <dtang859@gmail.com>
2016-05-16 09:27:27 -0700

    v2.0.7

--- dist/validator.js
+++ dist/validator.js
@@ -1,4 +1,4 @@
-/*! validatorjs - v2.0.6 -  - 2016-05-16 */
+/*! validatorjs - v2.0.7 -  - 2016-05-16 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Validator = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function AsyncResolvers(onFailedOne, onResolvedAll) {
   this.onResolvedAll = onResolvedAll;
@@ -144,7 +144,10 @@ Errors.prototype = {
     if (!this.has(attribute)) {
       this.errors[attribute] = [];
     }
-    this.errors[attribute].push(message);
+
+    if (this.errors[attribute].indexOf(message) === -1) {
+      this.errors[attribute].push(message);
+    }
   },
 
   /**
@@ -882,7 +885,6 @@ var manager = {
 module.exports = manager;
 
 },{}],8:[function(require,module,exports){
-// Get required modules
 var Rules = require('./rules');
 var Lang = require('./lang');
 var Errors = require('./errors');
