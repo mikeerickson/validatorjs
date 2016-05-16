d8cc65e0d021a5fc47b49cbbef10df367439584d
Houssam Haidar <houssam@sdelements.com>
2016-05-16 11:49:28 -0400

    Fix max error message wording

--- dist/validator.js
+++ dist/validator.js
@@ -1,4 +1,4 @@
-/*! validatorjs - v2.0.6 -  - 2016-05-14 */
+/*! validatorjs - v2.0.6 -  - 2016-05-16 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Validator = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function AsyncResolvers(onFailedOne, onResolvedAll) {
   this.onResolvedAll = onResolvedAll;
@@ -296,8 +296,8 @@ module.exports = {
     string: 'The :attribute must be at least :min characters.'
   },
   max: {
-    numeric: 'The :attribute must be less than :max.',
-    string: 'The :attribute must be less than :max characters.'
+    numeric: 'The :attribute may not be greater than :max.',
+    string: 'The :attribute may not be greater than :max characters.'
   },
   not_in: 'The selected :attribute is invalid.',
   numeric: 'The :attribute must be a number.',
