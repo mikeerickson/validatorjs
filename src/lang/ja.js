7d32b87fe3bc48d143f56bd32d5878793ee86ca4
Karol Janyst <lapkom@gmail.com>
2016-06-21 15:20:43 +0900

    Add japanese translation

---
+++ src/lang/ja.js
@@ -0,0 +1,34 @@
+module.exports = {
+    accepted: ':attributeを確認してください。',
+    alpha: ':attributeは英字のみで入力してください。',
+    alpha_dash: ':attributeは英字とダッシュと下線のみで入力してください。',
+    alpha_num: ':attributeは英数字のみで入力してください。',
+    between: ':attributeは:min〜:max文字で入力してください。',
+    confirmed: ':attributeは確認が一致しません。',
+    email: ':attributeは正しいメールアドレスを入力してください。',
+    def: ':attributeは検証エラーが含まれています。',
+    digits: ':attributeは:digitsの数字のみで入力してください。',
+    different: ':attributeと:differentは同じであってはなりません。',
+    'in': '選択された:attributeは無効です。',
+    integer: ':attributeは整数で入力してください。',
+    min        : {
+        numeric : ":attributeは:min以上を入力してください。",
+        string  : ":attributeは:min文字以上で入力してください。"
+    },
+    max : {
+        numeric : ":attributeは:max以下を入力してください。",
+        string  : ":attributeは:max文字以上で入力してください。"
+    },
+    not_in      : "選択された:attributeは無効です。",
+    numeric     : ":attributeは数値で入力してください。",
+    required    : ":attributeは必須です。",
+    required_if : ":otherは:valueになったら:attributeは必須です。",
+    same        : ":attributeと:sameは同じでなければなりません。",
+    size        : {
+        numeric : ":attributeは:sizeを入力してください。",
+        string  : ":attributeは:size文字で入力してください。"
+    },
+    url        : ":attributeはURIを入力してください。",
+    regex      : ":attributeの値 \":value\" はパターンにマッチする必要があります。",
+    attributes : {}
+};
