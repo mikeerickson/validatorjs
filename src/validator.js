08954e5960e52336e590be91bb0d323219ceba5e
Oscar Bezi <oscar@bezi.io>
2016-07-27 14:12:52 -0400

    Additional uncaught bug from #109 fixed.

--- src/validator.js
+++ src/validator.js
@@ -189,7 +189,7 @@ Validator.prototype = {
     }
     
     var keys = path.replace(/\[(\w+)\]/g, ".$1").replace(/^\./, "").split(".");
-    var copy = obj.constructor();
+    var copy = {};
 
     for (var attr in obj) {
       if (Object.prototype.hasOwnProperty.call(obj, attr)) {
@@ -299,7 +299,6 @@ Validator.prototype = {
 
     return this.getRule('required').validate(value);
   },
-
 
   /**
    * Determine if we should stop validating.
