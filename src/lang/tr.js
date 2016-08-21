5f84dc2c642a3f2ed3767e3fa6f39f1c2421dfdd
Ecmel Ercan <Ecmel Ercan>
2016-08-21 14:57:24 +0300

    Add Turkish (tr) localization

---
+++ src/lang/tr.js
@@ -0,0 +1,36 @@
+module.exports = {
+  accepted: ':attribute kabul edilmeli.',
+  alpha: ':attribute alanı sadece harflerden oluşabilir.',
+  alpha_dash: ':attribute alanı sadece alfa-nümerik, tire ve alt çizgi karakterlerden oluşabilir.',
+  alpha_num: ':attribute alanı alfa-nümerik olmalıdır.',
+  between: ':attribute alanı :min ile :max arasında olabilir.',
+  confirmed: ':attribute uyuşmuyor.',
+  email: ':attribute formatı geçersiz.',
+  date: ':attribute geöerli bir tarih alanı değil.',
+  def: ':attribute hatalar içeriyor.',
+  digits: ':attribute sadece rakamlardan oluşabilir.',
+  different: ':attribute ve :different farklı olmalı.',
+  'in': 'Seçilen :attribute geçerli değil.',
+  integer: ':attribute tam sayı olmalı.',
+  min: {
+    numeric: ':attribute en az :min olmalı.',
+    string: ':attribute en az :min karakter uzunluğunda olmalı.'
+  },
+  max: {
+    numeric: ':attribute en çok :max olabilir.',
+    string: ':attribute uzunluğu en çok :max karakter uzunluğunda olabilir.'
+  },
+  not_in: 'Seçilen :attribute geçerli değil.',
+  numeric: ':attribute sayı olmalı.',
+  required: ':attribute alanı gerekli.',
+  required_if: ':attribute alanı :other alanı :value olduğunda gerekli.',
+  same: ':attribute ve :same aynı olmalı.',
+  size: {
+    numeric: ':attribute :size olmalı.',
+    string: ':attribute :size karakter uzunluğunda olmalı.'
+  },
+  string: ':attribute alfa-numerik olmalı.',
+  url: ':attribute formatı geçersiz.',
+  regex: ':attribute formatı geçersiz.',
+  attributes: {}
+};
