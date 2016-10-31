98608a321edccd10113b4282015843a215a19fad
Khánh Trần <rualatngua@users.noreply.github.com>
2016-10-31 22:20:51 +0700

    Create vi.js
    Translate to Vietnamse.

---
+++ src/lang/vi.js
@@ -0,0 +1,36 @@
+module.exports = {
+  accepted: 'Trường :attribute phải được chấp nhận.',
+  alpha: 'Trường :attribute phải là số',
+  alpha_dash: 'Trường :attribute chỉ chấp nhận ký tự chữ cái, số, dấu gạch chéo và gạch dưới.',
+  alpha_num: 'Trường :attribute field must be alphanumeric.',
+  between: 'Trường :attribute phải nằm trong khoảng :min và :max.',
+  confirmed: 'The :attribute confirmation does not match.',
+  email: 'Trường :attribute không phải là một email hợp lệ.',
+  date: 'The :attribute is not a valid date format',
+  def: 'The :attribute attribute has errors.',
+  digits: 'The :attribute must be :digits digits.',
+  different: 'The :attribute and :different must be different.',
+  'in': 'The selected :attribute is invalid.',
+  integer: 'The :attribute must be an integer.',
+  min: {
+    numeric: 'The :attribute must be at least :min.',
+    string: 'The :attribute must be at least :min characters.'
+  },
+  max: {
+    numeric: 'The :attribute may not be greater than :max.',
+    string: 'The :attribute may not be greater than :max characters.'
+  },
+  not_in: 'The selected :attribute is invalid.',
+  numeric: 'The :attribute must be a number.',
+  required: 'Trường :attribute bắt buộc nhập.',
+  required_if: 'The :attribute field is required when :other is :value.',
+  same: 'The :attribute and :same fields must match.',
+  size: {
+    numeric: 'The :attribute must be :size.',
+    string: 'The :attribute must be :size characters.'
+  },
+  string: 'The :attribute must be a string.',
+  url: 'The :attribute format is invalid.',
+  regex: 'The :attribute format is invalid',
+  attributes: {}
+};
