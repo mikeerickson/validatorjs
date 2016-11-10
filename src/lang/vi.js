538226ca541e5b324c70e89b9cad4bf1b251a9ef
Khánh Trần <rualatngua@users.noreply.github.com>
2016-11-10 20:12:41 +0700

    some new translate

--- src/lang/vi.js
+++ src/lang/vi.js
@@ -1,36 +1,36 @@
 module.exports = {
-  accepted: 'Trường :attribute phải được chấp nhận.',
-  alpha: 'Trường :attribute phải là số',
-  alpha_dash: 'Trường :attribute chỉ chấp nhận ký tự chữ cái, số, dấu gạch chéo và gạch dưới.',
-  alpha_num: 'Trường :attribute field must be alphanumeric.',
-  between: 'Trường :attribute phải nằm trong khoảng :min và :max.',
+  accepted: ':attribute phải được chấp nhận.',
+  alpha: 'Trường :attribute phải là ký tự',
+  alpha_dash: ':attribute chỉ chấp nhận ký tự chữ cái, số, dấu gạch chéo và gạch dưới.',
+  alpha_num: ':attribute field must be alphanumeric.',
+  between: ':attribute phải nằm trong khoảng :min và :max.',
   confirmed: 'The :attribute confirmation does not match.',
-  email: 'Trường :attribute không phải là một email hợp lệ.',
+  email: ':attribute không phải là email.',
   date: 'The :attribute is not a valid date format',
   def: 'The :attribute attribute has errors.',
   digits: 'The :attribute must be :digits digits.',
   different: 'The :attribute and :different must be different.',
   'in': 'The selected :attribute is invalid.',
-  integer: 'The :attribute must be an integer.',
+  integer: 'Trường :attribute phải là số nguyên.',
   min: {
-    numeric: 'The :attribute must be at least :min.',
-    string: 'The :attribute must be at least :min characters.'
+    numeric: ':attribute phải lớn hơn hoặc bằng :min.',
+    string: ':attribute phải có ít nhất :min ký tự.'
   },
   max: {
-    numeric: 'The :attribute may not be greater than :max.',
-    string: 'The :attribute may not be greater than :max characters.'
+    numeric: ':attribute phải nhỏ hơn hoặc bằng :max.',
+    string: ':attribute phải có ít hơn :max ký tự.'
   },
   not_in: 'The selected :attribute is invalid.',
-  numeric: 'The :attribute must be a number.',
-  required: 'Trường :attribute bắt buộc nhập.',
-  required_if: 'The :attribute field is required when :other is :value.',
-  same: 'The :attribute and :same fields must match.',
+  numeric: ':attribute phải là số.',
+  required: ':attribute bắt buộc nhập.',
+  required_if: ':attribute là bắt buộc khi :other có giá trị :value.',
+  same: 'Giá trị của :attribute và :same phải như nhau.',
   size: {
     numeric: 'The :attribute must be :size.',
     string: 'The :attribute must be :size characters.'
   },
   string: 'The :attribute must be a string.',
   url: 'The :attribute format is invalid.',
-  regex: 'The :attribute format is invalid',
+  regex: ':attribute không đúng định dạng',
   attributes: {}
 };
