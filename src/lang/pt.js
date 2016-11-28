a3182841f2a7f5685729ee24b1b877b1a7f2abdd
Davi Chaves <dchaves@usc.edu>
2016-11-28 02:01:51 -0800

    portuguese translations

---
+++ src/lang/pt.js
@@ -0,0 +1,36 @@
+module.exports = {
+  accepted: 'O :attribute precisa ser aceito.',
+  alpha: 'O campo :attribute só pode conter letras.',
+  alpha_dash: 'O campo :attribute só pode conter letras, números, hífens e sublinha.',
+  alpha_num: 'O campo :attribute só pode conter letras e números.',
+  between: 'O campo :attribute precisa estar entre :min e :max.',
+  confirmed: 'A confirmação de :attribute não coincide.',
+  email: 'O formato de :attribute é inválido.',
+  date: 'O :attribute não é um formato de data válido',
+  def: 'O atributo :attribute contém erros.',
+  digits: 'O atributo :attribute precisa ter :digits dígitos.',
+  different: 'O :attribute e :different precisam ser diferentes.',
+  'in': 'O atributo selecionado :attribute é inválido.',
+  integer: 'O :attribute precisa ser um inteiro.',
+  min: {
+    numeric: 'O :attribute precisa ser no mínimo :min.',
+    string: 'O :attribute precisa ter no mínimo :min caracteres.'
+  },
+  max: {
+    numeric: 'O :attribute não pode ser maior que :max.',
+    string: 'O :attribute não pode ter mais que :max caracteres.'
+  },
+  not_in: 'O :attribute selecionado é inválido.',
+  numeric: 'O :attribute precisa ser um número.',
+  required: 'O campo :attribute é obrigatório.',
+  required_if: 'O campo :attribute é obrigatório quando :other é :value.',
+  same: 'Os campos :attribute e :same precisam ser iguais.',
+  size: {
+    numeric: 'O :attribute precisa ser :size.',
+    string: 'O :attribute precisa ter :size caracteres.'
+  },
+  string: 'O :attribute precisa ser uma palavra.',
+  url: 'O formato de :attribute é inválido.',
+  regex: 'O formato de :attribute é inválido.',
+  attributes: {}
+};
