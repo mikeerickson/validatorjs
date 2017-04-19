8eb232487225d58e6dfc2e88c44227907dd2ce24
Eugen Mueller <eugen.mueller@anwr-group.com>
2017-04-19 09:01:07 +0200

    Updated artifacts

--- dist/validator.js
+++ dist/validator.js
@@ -1,4 +1,4 @@
-/*! validatorjs - v3.11.0 -  - 2017-04-19 */
+/*! validatorjs - v3.12.0 -  - 2017-04-19 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Validator = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function AsyncResolvers(onFailedOne, onResolvedAll) {
   this.onResolvedAll = onResolvedAll;
@@ -1202,7 +1202,7 @@ var AsyncResolvers = require('./async');
 
 var Validator = function(input, rules, customMessages) {
   var lang = Validator.getDefaultLang();
-  this.input = input;
+  this.input = input || {};
 
   this.messages = Lang._make(lang);
   this.messages._setCustom(customMessages);
