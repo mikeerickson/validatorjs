97fae4560f9ce6d12b8e72628ef00554e26d0348
roxannedunn <rdunn@cavulus.com>
2017-05-18 13:24:56 -0700

    updated the tests after the period addition

--- dist/validator.js
+++ dist/validator.js
@@ -1,3 +1,4 @@
+/*! validatorjs - v3.12.0 -  - 2017-05-18 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Validator = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function AsyncResolvers(onFailedOne, onResolvedAll) {
   this.onResolvedAll = onResolvedAll;
@@ -419,7 +420,7 @@ module.exports = {
   between: 'The :attribute field must be between :min and :max.',
   confirmed: 'The :attribute confirmation does not match.',
   email: 'The :attribute format is invalid.',
-  date: 'The :attribute is not a valid date format',
+  date: 'The :attribute is not a valid date format.',
   def: 'The :attribute attribute has errors.',
   digits: 'The :attribute must be :digits digits.',
   different: 'The :attribute and :different must be different.',
@@ -450,7 +451,7 @@ module.exports = {
   },
   string: 'The :attribute must be a string.',
   url: 'The :attribute format is invalid.',
-  regex: 'The :attribute format is invalid',
+  regex: 'The :attribute format is invalid.',
   attributes: {}
 };
 
@@ -930,7 +931,7 @@ var rules = {
   date: function(val, format) {
     return isValidDate(val);
   },
-  
+
   present: function(val) {
     return typeof val !== 'undefined';
   },
