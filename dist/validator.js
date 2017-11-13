b55c1ddb5044c9ae29b156d0db304c7029b2e56c
Johan Hillerström <progr@mmer.nu>
2017-11-13 13:33:01 +0100

    Get attribute names for replacement fields

--- dist/validator.js
+++ dist/validator.js
@@ -1,4 +1,4 @@
-/*! validatorjs - v3.13.5 -  - 2017-10-09 */
+/*! validatorjs - v3.13.5 -  - 2017-11-13 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Validator = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function AsyncResolvers(onFailedOne, onResolvedAll) {
   this.onResolvedAll = onResolvedAll;
@@ -110,7 +110,7 @@ var replacements = {
   required_if: function(template, rule) {
     var parameters = rule.getParameters();
     return this._replacePlaceholders(rule, template, {
-      other: parameters[0],
+      other: this._getAttributeName(parameters[0]),
       value: parameters[1]
     });
   },
@@ -125,7 +125,7 @@ var replacements = {
   required_unless: function(template, rule) {
     var parameters = rule.getParameters();
     return this._replacePlaceholders(rule, template, {
-      other: parameters[0],
+      other: this._getAttributeName(parameters[0]),
       value: parameters[1]
     });
   },
@@ -140,7 +140,7 @@ var replacements = {
   required_with: function(template, rule) {
     var parameters = rule.getParameters();
     return this._replacePlaceholders(rule, template, {
-      field: parameters[0]
+      field: this._getAttributeName(parameters[0])
     });
   },
 
@@ -153,8 +153,9 @@ var replacements = {
    */
   required_with_all: function(template, rule) {
     var parameters = rule.getParameters();
+    var getAttributeName = this._getAttributeName.bind(this);
     return this._replacePlaceholders(rule, template, {
-      fields: parameters.join(', ')
+      fields: parameters.map(getAttributeName).join(', ')
     });
   },
 
@@ -168,7 +169,7 @@ var replacements = {
   required_without: function(template, rule) {
     var parameters = rule.getParameters();
     return this._replacePlaceholders(rule, template, {
-      field: parameters[0]
+      field: this._getAttributeName(parameters[0])
     });
   },
 
@@ -181,8 +182,9 @@ var replacements = {
    */
   required_without_all: function(template, rule) {
     var parameters = rule.getParameters();
+    var getAttributeName = this._getAttributeName.bind(this);
     return this._replacePlaceholders(rule, template, {
-      fields: parameters.join(', ')
+      fields: parameters.map(getAttributeName).join(', ')
     });
   },
 
