8f680074b006279a175cd5cb32035dd2d20a91c1
Mike Erickson <codedungeon@gmail.com>
2018-10-26 10:34:07 -0700

    031501/update browser testing (#297)
    * v3.15.0
    
    * 0315/update browser testing
    
    * fixes issues with browser testing, now using chrome headless and puppeteer

--- karma.conf.js
+++ karma.conf.js
@@ -1,64 +1,48 @@
-// Karma configuration
-// Generated on Sat Jul 12 2014 14:48:42 GMT-0700 (PDT)
+const puppeteer = require("puppeteer");
+process.env.CHROME_BIN = puppeteer.executablePath();
 
 module.exports = function(config) {
   config.set({
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
+    basePath: "",
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
-
+    frameworks: ["mocha", "chai"],
 
     // list of files / patterns to load in the browser
-    files: [
-      'dist/validator.js',
-      'spec/*.js'
-    ],
-
+    files: ["dist/validator.js", "spec/*.js"],
 
     // list of files to exclude
-    exclude: [
-
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'dist/validator.js': ['coverage']
+      "dist/validator.js": ["coverage"]
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage', 'growl-notifications'],
-
+    reporters: ["progress", "coverage", "growl-notifications"],
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
-
+    browsers: ["ChromeHeadless"],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
