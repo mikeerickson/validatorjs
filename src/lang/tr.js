02bb0de26eaad4366145d7729f1fd6e2b3d37d50
emin şen <emnsen@users.noreply.github.com>
2019-12-04 07:13:52 +0100

    Update Turkish Language (#353)
    * Update Turkish Language
    
    * quotes and translation
    
    double quotes converted to single quotes
    hex rule translated

--- src/lang/tr.js
+++ src/lang/tr.js
@@ -1,8 +1,12 @@
 module.exports = {
   accepted: ':attribute kabul edilmeli.',
+  after: ':attribute alanı :after alanından sonra olmalıdır.',
+  after_or_equal: ':attribute alanı :after_or_equal alanına eşit veya sonra olmalıdır.',
   alpha: ':attribute alanı sadece harflerden oluşabilir.',
   alpha_dash: ':attribute alanı sadece alfa-nümerik, tire ve alt çizgi karakterlerden oluşabilir.',
   alpha_num: ':attribute alanı alfa-nümerik olmalıdır.',
+  before: ':attribute alanı :before alanından önce olmalıdır.',
+  before_or_equal: ':attribute alanı :before_or_equal alanına eşit veya önce olmalıdır.',
   between: ':attribute alanı :min ile :max arasında olabilir.',
   confirmed: ':attribute uyuşmuyor.',
   email: ':attribute formatı geçersiz.',
@@ -13,7 +17,7 @@ module.exports = {
   different: ':attribute ve :different farklı olmalı.',
   in: 'Seçilen :attribute geçerli değil.',
   integer: ':attribute tam sayı olmalı.',
-  hex: 'The :attribute should have hexadecimal format',
+  hex: ':attribute onaltılık formatta olmalı.',
   min: {
     numeric: ':attribute en az :min olmalı.',
     string: ':attribute en az :min karakter uzunluğunda olmalı.'
@@ -27,6 +31,11 @@ module.exports = {
   present: ':attribute alanı bulunmalıdır (ancak boş olabilir).',
   required: ':attribute alanı gerekli.',
   required_if: ':attribute alanı :other alanı :value olduğunda gerekli.',
+  required_unless: ':attribute alanı :other alanı :value değilse gereklidir.',
+  required_with: ':attribute alanı :field boş değilse gereklidir.',
+  required_with_all: ':attribute alanı :fields alanları boş değilse gereklidir.',
+  required_without: ':attribute alanı :field alanı boşsa gereklidir.',
+  required_without_all: ':attribute alanı :fields alanları boşsa gereklidir.',
   same: ':attribute ve :same aynı olmalı.',
   size: {
     numeric: ':attribute :size olmalı.',
