c2bb2e39d8958b7a6368237b0dc45edd9a675cd4
CSenshi <saba.pochkhua@gmail.com>
2020-04-28 20:43:08 +0400

    add tests for ipv4

--- spec/ip.js
+++ spec/ip.js
@@ -7,7 +7,88 @@ if (typeof require !== 'undefined') {
 }
 
 describe('IP Validation rules', function () {
+
   describe('IPv4 Validation rule', function () {
+    it('should accept localhost ipv4 addres', function () {
+      var validator = new Validator({
+        ipAddr: '127.0.0.1'
+      }, {
+        ipAddr: 'ipv4'
+      });
+      expect(validator.passes()).to.be.true;
+    });
+
+    it('should accept maximum ip range ', function () {
+      var validator = new Validator({
+        ipAddr: '255.255.255.255'
+      }, {
+        ipAddr: 'ipv4'
+      });
+      expect(validator.passes()).to.be.true;
+    });
+
+    it('should accept random ip address withing range ', function () {
+      var validator = new Validator({
+        ipAddr: '192.168.33.10'
+      }, {
+        ipAddr: 'ipv4'
+      });
+      expect(validator.passes()).to.be.true;
+    });
+
+    it('should fail ip address containing non integer octants', function () {
+      var validator = new Validator({
+        ipAddr: '192.fail.33.10'
+      }, {
+        ipAddr: 'ipv4'
+      });
+      expect(validator.passes()).to.be.fail;
+    });
+
+    it('should fail ip address containing non integer octants', function () {
+      var validator = new Validator({
+        ipAddr: '192.fail.33.10'
+      }, {
+        ipAddr: 'ipv4'
+      });
+      expect(validator.passes()).to.be.fail;
+    });
+
+    it('should fail ip address more than 4 octants', function () {
+      var validator = new Validator({
+        ipAddr: '192.168.33.10.10'
+      }, {
+        ipAddr: 'ipv4'
+      });
+      expect(validator.passes()).to.be.fail;
+    });
+
+    it('should fail ip address less than 4 octants', function () {
+      var validator = new Validator({
+        ipAddr: '192.168.10'
+      }, {
+        ipAddr: 'ipv4'
+      });
+      expect(validator.passes()).to.be.fail;
+    });
+
+    it('should fail ip address which contains octant values more than 255', function () {
+      var validator = new Validator({
+        ipAddr: '256.168.10.0'
+      }, {
+        ipAddr: 'ipv4'
+      });
+      expect(validator.passes()).to.be.fail;
+    });
+
+    it('should fail ip address which contains empty octants', function () {
+      var validator = new Validator({
+        ipAddr: '256...0'
+      }, {
+        ipAddr: 'ipv4'
+      });
+      expect(validator.passes()).to.be.fail;
+    });
   });
 
   describe('IPv6 Validation rule', function () {
