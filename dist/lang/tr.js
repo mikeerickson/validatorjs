1610bf19c7eb7e5ead4d0d38308f5cffe2b12fe1
igor <igor875126@gmail.com>
2020-08-18 12:14:49 +0600

    Test added + fresh build

--- dist/lang/tr.js
+++ dist/lang/tr.js
@@ -1,9 +1,13 @@
 require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({"./lang/tr":[function(require,module,exports){
 module.exports = {
   accepted: ':attribute kabul edilmeli.',
+  after: ':attribute alanı :after alanından sonra olmalıdır.',
+  after_or_equal: ':attribute alanı :after_or_equal alanına eşit veya sonra olmalıdır.',
   alpha: ':attribute alanı sadece harflerden oluşabilir.',
   alpha_dash: ':attribute alanı sadece alfa-nümerik, tire ve alt çizgi karakterlerden oluşabilir.',
   alpha_num: ':attribute alanı alfa-nümerik olmalıdır.',
+  before: ':attribute alanı :before alanından önce olmalıdır.',
+  before_or_equal: ':attribute alanı :before_or_equal alanına eşit veya önce olmalıdır.',
   between: ':attribute alanı :min ile :max arasında olabilir.',
   confirmed: ':attribute uyuşmuyor.',
   email: ':attribute formatı geçersiz.',
@@ -14,7 +18,7 @@ module.exports = {
   different: ':attribute ve :different farklı olmalı.',
   in: 'Seçilen :attribute geçerli değil.',
   integer: ':attribute tam sayı olmalı.',
-  hex: 'The :attribute should have hexadecimal format',
+  hex: ':attribute onaltılık formatta olmalı.',
   min: {
     numeric: ':attribute en az :min olmalı.',
     string: ':attribute en az :min karakter uzunluğunda olmalı.'
@@ -28,6 +32,11 @@ module.exports = {
   present: ':attribute alanı bulunmalıdır (ancak boş olabilir).',
   required: ':attribute alanı gerekli.',
   required_if: ':attribute alanı :other alanı :value olduğunda gerekli.',
+  required_unless: ':attribute alanı :other alanı :value değilse gereklidir.',
+  required_with: ':attribute alanı :field boş değilse gereklidir.',
+  required_with_all: ':attribute alanı :fields alanları boş değilse gereklidir.',
+  required_without: ':attribute alanı :field alanı boşsa gereklidir.',
+  required_without_all: ':attribute alanı :fields alanları boşsa gereklidir.',
   same: ':attribute ve :same aynı olmalı.',
   size: {
     numeric: ':attribute :size olmalı.',
--- spec/digits-rule.js
+++ spec/digits-rule.js
@@ -29,4 +29,26 @@ describe('digits rule', function() {
     expect(validation.errors.first('zip')).to.equal('The zip must be 5 digits.');
     expect(validation.passes()).to.be.false;
   });
+
+  it('should not pass if spaces are present', function() {
+    var validation = new Validator({
+      zip: '9098 '
+    }, {
+      zip: 'digits:5'
+    });
+
+    expect(validation.fails()).to.be.true;
+    expect(validation.errors.first('zip')).to.equal('The zip must be 5 digits.');
+    expect(validation.passes()).to.be.false;
+
+    var validation = new Validator({
+      zip: ' 9098'
+    }, {
+      zip: 'digits:5'
+    });
+
+    expect(validation.fails()).to.be.true;
+    expect(validation.errors.first('zip')).to.equal('The zip must be 5 digits.');
+    expect(validation.passes()).to.be.false;
+  });
 });
