a2dda179acc3d0c039ddf7f66fa588dc8a3c3e13
castiman <castiman@users.noreply.github.com>
2021-02-15 17:49:14 +0100

    Added object rules

--- src/rules.js
+++ src/rules.js
@@ -255,6 +255,10 @@ var rules = {
   array: function (val) {
     return val instanceof Array;
   },
+  
+  object: function (val) {
+    return val instanceof Object;
+  },
 
   url: function (url) {
     return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{2,63}\b([-a-zA-Z0-9@:%_\+.~#?&/=]*)/i.test(url);
