27225333f243bd421bbd654d2627e7a412e1f14a
Ndn1618 <ndilbar1618@gmail.com>
2021-06-18 18:15:35 +0500

    Add build version

--- src/lang/uz.js
+++ src/lang/uz.js
@@ -1,4 +1,3 @@
-require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({"./lang/uz":[function(require,module,exports){
 module.exports = {
   accepted: 'Siz :attribute ni qabul qilishingiz kerak.',
   alpha: ':attribute maydoni faqat harflardan iborat bo`lishi mumkin.',
@@ -36,5 +35,3 @@ module.exports = {
   regex: ':attribute maydon formati noto`g`ri.',
   attributes: {}
 };
-
-},{}]},{},[]);
