2607344f74b63d777d58af3be68f97ef0c6c102f
Yoswaris Lawpaiboon <22832362+kiznick@users.noreply.github.com>
2021-09-09 21:01:38 +0700

    edit email regex algorithm
    Old : If pass regex 1 return function will run regex again
    Edit : If pass regex 1 return

--- src/rules.js
+++ src/rules.js
@@ -233,10 +233,11 @@ var rules = {
   email: function (val) {
     // Added umlaut support https://github.com/skaterdav85/validatorjs/issues/308
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+\.)+[a-zA-Z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]{2,}))$/;
-    if (!re.test(val)) {
-      // added support domain 3-n level https://github.com/skaterdav85/validatorjs/issues/384
-      re = /^((?:[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]|[^\u0000-\u007F])+@(?:[a-zA-Z0-9]|[^\u0000-\u007F])(?:(?:[a-zA-Z0-9-]|[^\u0000-\u007F]){0,61}(?:[a-zA-Z0-9]|[^\u0000-\u007F]))?(?:\.(?:[a-zA-Z0-9]|[^\u0000-\u007F])(?:(?:[a-zA-Z0-9-]|[^\u0000-\u007F]){0,61}(?:[a-zA-Z0-9]|[^\u0000-\u007F]))?)+)*$/;
+    if (re.test(val)) {
+      return true;
     }
+    // added support domain 3-n level https://github.com/skaterdav85/validatorjs/issues/384
+    re = /^((?:[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]|[^\u0000-\u007F])+@(?:[a-zA-Z0-9]|[^\u0000-\u007F])(?:(?:[a-zA-Z0-9-]|[^\u0000-\u007F]){0,61}(?:[a-zA-Z0-9]|[^\u0000-\u007F]))?(?:\.(?:[a-zA-Z0-9]|[^\u0000-\u007F])(?:(?:[a-zA-Z0-9-]|[^\u0000-\u007F]){0,61}(?:[a-zA-Z0-9]|[^\u0000-\u007F]))?)+)*$/;
     return re.test(val);
   },
 
